e21f5a9c4d956557039f9bf72b1ee6cb26f1dc5f
Anais <anaiszed447@gmail.com>
2021-11-08 18:15:20 -0500

    Initial Commit

---
+++ OneShopAnais/OneShopAnaisApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneShopAnaisApp.swift
+//  OneShopAnais
+//
+//  Created by Anais Zulfequar on 2021-11-08.
+//
+
+import SwiftUI
+
+@main
+struct OneShopAnaisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
