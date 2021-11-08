d00ddeb22bc2f5871e0160880563b00bc71b1c63
Mobin Akhter <mobinakhter18@gmail.com>
2021-11-08 17:51:10 +0100

    Initial Commit

---
+++ OneShop/OneShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneShopApp.swift
+//  OneShop
+//
+//  Created by user198375 on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct OneShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
