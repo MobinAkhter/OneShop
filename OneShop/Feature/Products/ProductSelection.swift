5a9d104cbd3e5cbdb26d8a4b56d47b23798bc49a
Anais <anaiszed447@gmail.com>
2021-11-20 00:26:02 -0500

    Deleted folders and added new folders

---
+++ OneShop/Feature/Products/ProductSelection.swift
@@ -0,0 +1,20 @@
+//
+//  ProductSelection.swift
+//  OneShop
+//
+//  Created by Anais Zulfequar on 2021-11-20.
+//
+
+import SwiftUI
+
+struct ProductSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductSelection_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductSelection()
+    }
+}
