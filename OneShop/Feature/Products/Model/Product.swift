497ba7eeb42b02f74123882c1dbfe5e4efc1e5cc
Anais <anaiszed447@gmail.com>
2021-11-22 14:15:50 -0500

    Implemented the logic for the Product Model

--- OneShop/Feature/Products/Model/Product.swift
+++ OneShop/Feature/Products/Model/Product.swift
@@ -6,4 +6,31 @@
 //
 
 import Foundation
-//random comment
+import FirebaseFirestoreSwift
+
+struct Product: Hashable, Codable{
+    @DocumentID var id : String? = UUID().uuidString
+    var productName : String = ""
+    var category : String = ""
+    
+    init(){
+        
+    }
+    
+    init(productName: String, category: String){
+        self.productName = productName
+        self.category = category
+    }
+    
+    init?(dictionary: [String : Any]){
+        guard let productName = dictionary["productName"] as? String else{
+            return nil
+        }
+        
+        guard let category = dictionary["category"] as? String else{
+            return nil
+        }
+        
+        self.init(productName: productName, category: category)
+    }
+}
