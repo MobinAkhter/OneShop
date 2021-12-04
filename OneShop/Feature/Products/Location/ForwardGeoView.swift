e8ecf6f80211a1db7b8ba555a64852f627c263f0
Anais <anaiszed447@gmail.com>
2021-12-04 09:27:57 -0500

    Created Swift UI to access user's location.

---
+++ OneShop/Feature/Products/Location/ForwardGeoView.swift
@@ -0,0 +1,20 @@
+//
+//  ForwardGeoView.swift
+//  OneShop
+//
+//  Created by Anais Zulfequar on 2021-12-04.
+//
+
+import SwiftUI
+
+struct ForwardGeoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ForwardGeoView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForwardGeoView()
+    }
+}
