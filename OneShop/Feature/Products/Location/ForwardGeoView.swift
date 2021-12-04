40347cccd4e6337e010b2679778417e187267d79
Anais <anaiszed447@gmail.com>
2021-12-04 09:46:35 -0500

    Created the logic to take user's address and covert them into location coordinates.

--- OneShop/Feature/Products/Location/ForwardGeoView.swift
+++ OneShop/Feature/Products/Location/ForwardGeoView.swift
@@ -6,10 +6,66 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ForwardGeoView: View {
+    @State private var street: String = ""
+    @State private var city : String = ""
+    @State private var country: String = ""
+    @State private var result : String = ""
+    @ObservedObject var locationHelper = LocationHelper()
+    @State private var obtainedCoordinates: CLLocation?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Enter Your Coordinates")
+                .foregroundColor(.red)
+                .fontWeight(.bold)
+                .font(.system(size: 30))
+                .padding()
+            
+            Form{
+                TextField("Enter Your Street", text: $street)
+                
+                TextField("Enter Your City", text: $city)
+                
+                TextField("Enter Your Country", text: $country)
+            }
+            
+            Text("\(self.result)")
+                .foregroundColor(.red)
+                .fontWeight(.bold)
+                .font(.system(size: 30))
+                .padding()
+            
+            Text("Enter these coordinates to your phone")
+            
+            Button(action: {
+                let address = "\(self.street), \(self.city), \(self.country)"
+            }){
+                Text("Enter")
+            }
+            
+            Spacer()
+
+        }
+    }
+    
+    private func performGeocoding(address: String){
+        print(#function, "Performing geocoding on the address: \(address)")
+        
+        self.locationHelper.doForwardGeocoding(address: address, completionHandler: {(coordinates, error) in
+            
+            if (error == nil && coordinates != nil){
+                self.obtainedCoordinates = coordinates!
+                
+                self.result = "The latititude is \(coordinates!.coordinate.latitude) \n The longitude is : \(coordinates!.coordinate.longitude)"
+                
+                
+            }else{
+                self.result = "Unable to get the coordinates for given address"
+            }
+        })
     }
 }
 
